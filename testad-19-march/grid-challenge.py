bd37fcc11ef179a6a693c50f1fbaedeac3f746ef
gupta-ronak12 <ronak.gupta2427@gmail.com>
2026-03-20 00:08:42 +0530

    .

---
+++ testad-19-march/grid-challenge.py
@@ -0,0 +1,12 @@
+def gridChallenge(grid):
+    grid = [list(row) for row in grid]
+    r = len(grid)
+    c = len(grid[0])
+    for i in range (r):
+        grid[i].sort()
+    
+    for j in range(c):
+        for i in range(1, r):
+            if not grid[i - 1][j] <= grid[i][j]:
+                return "NO"
+    return "YES"
